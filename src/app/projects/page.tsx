3ef4a8f443129698e5654b08c4f9a9f8236c7722
your-username <dreamscaatcher@gmail.com>
2024-07-12 06:04:03 +0200

    first commit

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,17 @@ import ProjectCards from "@/components/ProjectsCard";
 import { Badge } from "@/components/ui/badge";
 import { Layers } from "lucide-react";
 
-const projectsPage = () => {
+// PROJECTS PAGE COMPONENT
+const ProjectsPage = () => {
+
+  interface BadgeProps {
+    // Other properties already defined in BadgeProps
+    // ...
+  
+    // Add this line to include className
+    className?: string;
+  }
+
   // PROJECTS DATA
   const Projects = [
     {
@@ -42,18 +52,17 @@ const projectsPage = () => {
       tags: ["Nextjs", "Freelancing", "MySQL"],
       link: "https://rupeespot.com/",
     },
-    
     {
       title: "KryptoKapital- Investing in Cryptocurreny",
       description:
-        "This is a platform for learning about cryptocurrency with many tools and festures. It is very big project. ",
+        "This is a platform for learning about cryptocurrency with many tools and features. It is a very big project. ",
       tags: ["Nextjs", "Freelancing", "Supabase"],
       link: "https://devkryptokapital.netlify.app/",
     },
     {
       title: "ChatApp- Simple instagram clone",
       description:
-        "ChatApp is similar to instagram, you can create, edit, delete, like,comment, post and  Follow, unfollow users",
+        "ChatApp is similar to instagram, you can create, edit, delete, like, comment, post and follow, unfollow users",
       tags: ["Reactjs", "Javascript"],
       link: "https://github.com/taqui-786/chatapp-api",
     },
@@ -75,31 +84,32 @@ const projectsPage = () => {
 
   return (
     // PROJECT PAGE
-    <div className="h-full w-full relative flex flex-col items-start gap-5 overflow-hidden">
-      <Badge className=" gap-2">
-        <Layers className="h-5 w-5" />
-        Projects
-      </Badge>
-      <div className="flex flex-col gap-3">
-        <Heading>My Projects</Heading>
-        <FramerWrapper y={0} x={200}>
-          <p className=" font-poppins text-lg w-full text-primary max-sm:text-base">
-            I love to Build Cool Projects. Here, you&#x27;ll find a curated
-            collection of my creative endeavors and technical projects. Each
-            piece represents a journey of innovation, problem-solving, and
-            continuous learning. Feel free to explore this showcase of my
-            passion and expertise in action.
-          </p>
-        </FramerWrapper>
+    <>
+      <div className="h-full w-full relative flex flex-col items-start gap-5 overflow-hidden">
+        <Badge className="gap-2">
+          <Layers className="h-5 w-5" />
+          Projects
+        </Badge>
+        <div className="flex flex-col gap-3">
+          <Heading>My Projects</Heading>
+          <FramerWrapper y={0} x={200}>
+            <p className="font-poppins text-lg w-full text-primary max-sm:text-base">
+              I love to Build Cool Projects. Here, you&#x27;ll find a curated
+              collection of my creative endeavors and technical projects. Each
+              piece represents a journey of innovation, problem-solving, and
+              continuous learning. Feel free to explore this showcase of my
+              passion and expertise in action.
+            </p>
+          </FramerWrapper>
+        </div>
+        <div className="w-full flex flex-row flex-wrap gap-3 max-lg:flex-col">
+          {Projects.map((val, indx) => {
+            return <ProjectCards key={indx} value={val} num={indx} />;
+          })}
+        </div>
       </div>
-
-      <div className=" w-full flex flex-row flex-wrap gap-3 max-lg:flex-col">
-        {Projects.map((val, indx) => {
-          return <ProjectCards key={indx} value={val} num={indx} />;
-        })}
-      </div>
-    </div>
+    </>
   );
 };
 
-export default projectsPage;
+export default ProjectsPage;
