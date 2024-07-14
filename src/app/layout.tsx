9623c525d4f59f588283dc4334f0fbb4d85f4106
your-username <dreamscaatcher@gmail.com>
2024-07-14 03:31:20 +0000

    ohne

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,16 +63,7 @@ export const metadata: Metadata = {
   creator: "Taqui imam",
 
   
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [`${siteConfig.url}/og-image.png`],
-    creator: "@Taquiimam14",
-  },
-  icons: {
-    icon: "/favicon.ico",
-  },
+ 
 };
 
 export default function RootLayout({
